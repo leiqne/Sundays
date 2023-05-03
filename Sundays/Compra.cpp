e9ab6ba6ceca177f59323eb7ea06297abfbdbc93
Brunix3017 <brunomedinaagnini@gmail.com>
2023-05-03 11:31:50 -0500

    Comentarios

--- Sundays/Compra.cpp
+++ Sundays/Compra.cpp
@@ -10,21 +10,21 @@ Compra::Compra() = default;
 
 Compra::Compra(Cliente client, const CarritoDeCompras& carrito_) : cliente(client), carrito(carrito_) {};
 
-void Compra::Guardar(){
-	csv::Parser file = csv::Parser(filename);
-	UUID = Cripto::generateUUID();
+void Compra::Guardar(){									
+	csv::Parser file = csv::Parser(filename); //historial.csv
+	UUID = Cripto::generateUUID();         //generamos el codigo de compra
 
-	auto t = std::time(nullptr);
-	auto tm = *std::localtime(&t);
+	auto t = std::time(nullptr);					//hora
+	auto tm = *std::localtime(&t);			//para saber la hora actual
 
 	std::ostringstream oss;
-	oss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");//para formatea la fecha
+	oss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");//para formatear la fecha cada vez que se haga una nueva compra
 	string fecha = oss.str();
 
-	auto row = { UUID, cliente.getUUID(), carrito.exportar(), to_string(carrito.total()), fecha };
+	auto row = { UUID, cliente.getUUID(), carrito.exportar(), to_string(carrito.total()), fecha }; //agregar a la fila todos los datos
 	
-	file.addRow(file.rowCount(), row);
-	file.sync();
+	file.addRow(file.rowCount(), row);		//se agrega la fila creada anteriormente
+	file.sync();                                               //se agrega todos los datos al archivo
 }
 
 Compra Compra::load(MiVector<Producto>& productos, string const data []) {
